eee0947e60388d4c6827d399e2642b7f3c945bd8
forough safari <forough.safari1986@gmail.com>
2021-07-29 18:36:09 +0430

    .gitignore commit

--- HelloWorld/src/com/HelloWorld/HelloWorld.java
+++ HelloWorld/src/com/HelloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("I am using Eclipse!");
+		System.out.println("I am using Eclipse for firsst in!");
 	}
 
 }
